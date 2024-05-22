73a103d8853210baf4ce800f71826803d8c2180f
Shahrriar Haque <shahriarhaque445@gmail.com>
2024-05-22 14:40:29 +0600

    Add Department Json

--- Employee Information/Department.cs
+++ Employee Information/Department.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,23 +12,9 @@ namespace Employee_Information
         public int DepartmentId { get; set; }
         public string DepartmentName { get; set; }
         public bool IsActive { get; set; }
+       
+       
 
-    
-        public Department(int id, string Dname, bool isactive)
-        {
-            DepartmentId = id;
-                DepartmentName = Dname;
-            IsActive = isactive;
-        }
-        public void Departmentdetails()
-        {
-            List<Department> list = new List<Department>();
-            list.Add(new Department(001, "HR", true));
-            list.Add(new Department(002, "IT", true));
-            list.Add(new Department(003, "Business", false));
-            list.Add(new Department(004, "Admin", true));
-           
-        }
     }
-   
+
 }
--- Employee Information/Program.cs
+++ Employee Information/Program.cs
@@ -1,9 +1,31 @@
 ﻿using Employee_Information;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using System;
 
 public class Program
 {
+    static List<Department> LoadDepartmentsFromJson(string filePath)
+    {
+        var json = File.ReadAllText("C:\\Users\\Hameem\\Desktop\\Intern - Shahriar Haque (Shipon)\\C# Console\\Employee Information\\Employee Information\\Department.json");
+        return JsonConvert.DeserializeObject<List<Department>>(json);
+    }
     public static void Main(string[] args)
     {
+
+        string jsonFilePath = "Department.json";
+        var departments = LoadDepartmentsFromJson(jsonFilePath);
+
+        foreach (var department in departments)
+        {
+            Console.WriteLine($"DepartmentId: {department.DepartmentId}");
+            Console.WriteLine($"DepartmentName: {department.DepartmentName}");
+            Console.WriteLine($"IsActive: {department.IsActive}");
+            Console.WriteLine();
+        }
+
+
+
         //creating employee list
         List<Employee> employees = new List<Employee>();
 
@@ -26,9 +48,6 @@ public class Program
 
             Console.Write("Enter Emplyee Date of Birth : ");
             string dof = Console.ReadLine();
-
-
-
             Console.Write("Enter Employee Date of Joing : ");
             string doj = Console.ReadLine();
 
@@ -45,22 +64,17 @@ public class Program
             employees.Add(new Employee(id, name, code, dof, doj, blood, dId, active));
 
         }
-       
-
-       
 
 
         foreach (var employee in employees)
         {
 
             Console.WriteLine($" Employee Id :{employee.Id} \n Employee Name:  {employee.Name} \n Employee Code : { employee.Code}\n Employee Department: {employee.DepartmentId}\n Employee Blood Group:{employee.BloodGroup}\n Employee Date of Joing : {employee.DateofJoing}\n Employee Date of Birth:{employee.Dob}\n Employee Active/InActive: {employee.IsActive}");
+            Console.WriteLine
+                ();
         }
-
-
-
-        List<Department> department = new List<Department>();
-      
-
        
     }
+
+    
 }
